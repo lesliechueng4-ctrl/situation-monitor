d15855b5053b2ca12807f6c03302575c2d30af1a
sjin5 <jincheng00@outlook.com>
2026-02-26 23:59:58 +0800

    Force add build directory for GitHub Pages

---
+++ build/_app/immutable/chunks/CDbpgISz.js
@@ -0,0 +1,1 @@
+import{b as p,E as t}from"./CzD5w6bx.js";import{B as c}from"./DJKsbOzh.js";function E(r,s,...a){var e=new c(r);p(()=>{const n=s()??null;e.ensure(n,n&&(o=>n(o,...a)))},t)}export{E as s};
